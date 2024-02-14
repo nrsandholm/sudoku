8465f81945593c5ed6605cb8670a6f7a539bbee0
Niko <nrsandholm@gmail.com>
2024-02-15 01:50:11 +0200

    Implement sudoku stack

--- infra/bin/sudoku.ts
+++ infra/bin/sudoku.ts
@@ -6,6 +6,7 @@ import { SudokuStack } from '../lib/sudoku-stack';
 const app = new cdk.App();
 
 new SudokuStack(app, 'SudokuStack', {
+  bucketName: 'sudoku-2154b72ff2',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
--- infra/lib/sudoku-stack.ts
+++ infra/lib/sudoku-stack.ts
@@ -1,8 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
+import {
+  CfnOutput,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  aws_s3,
+  aws_s3_deployment,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class SudokuStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface SudokuStackProps extends StackProps {
+  bucketName: string
+}
+
+export class SudokuStack extends Stack {
+  constructor(scope: Construct, id: string, props: SudokuStackProps) {
     super(scope, id, props);
+
+    const bucket = new aws_s3.Bucket(this, 'WebAppBucket', {
+      bucketName: props.bucketName,
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+    });
+
+    const deployment = new aws_s3_deployment.BucketDeployment(this, 'WebAppDeployment', {
+      destinationBucket: bucket,
+      sources: [
+        aws_s3_deployment.Source.asset('../app/sudoku/build/')
+      ],
+    });
+
+    new CfnOutput(this, 'WebAppUrl', {
+      value: deployment.deployedBucket.bucketWebsiteUrl
+    });
   }
 }
